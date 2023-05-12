002689a7887d493dca368518c316f154015bd635
sbaldu <simone.balducci00@gmail.com>
2023-05-12 09:33:56 +0200

    Add compilation and cleaning to install script

--- install_cxxgraph.sh
+++ install_cxxgraph.sh
@@ -3,9 +3,19 @@
 install_dir="~/.local/share/"
 path_to_packer="CXXGraph/packaging/"
 
+# Clone the repo of CXXGraph
 git clone --depth 1 https://github.com/ZigRazor/CXXGraph.git $install_dir
 
 cd $install_dir/$path_to_packer ;
+# Compile 
+mkdir -p build ;
+cd build ;
+cmake .. ; make ;
+
+# Install CXXGraph
 ./tarballs ; 
 sudo tar xjf CXXGraph--.tar.bz2 ;
 sudo cp -r usr /
+
+# Cleaning
+rm -r $install_dir/CXXGraph
