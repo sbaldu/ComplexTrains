a9550755918c3d3dfecbde4d08d80d0635abede0
sbaldu <simone.balducci00@gmail.com>
2023-05-15 15:54:25 +0200

    Add id attribute for trains and change type of itinerary elements

--- src/train.hpp
+++ src/train.hpp
@@ -1,29 +1,52 @@
 
 #pragma once
 
+#include <string>
 #include <vector>
 
 class Train {
   private:
 	int m_station;
 	bool m_prioritary;
-	std::vector<int> m_itinerary;
+	std::string m_id;
+	std::vector<std::string> m_itinerary;
 	
   public:
 	Train() = default;
 	Train(int station, bool prioritary)
 	  : m_station{station}, m_prioritary{prioritary} {}
-	Train(int station, bool prioritary, std::vector<int> itinerary)
+	Train(int station, bool prioritary, std::vector<std::string> itinerary)
 	  : m_station{station}, m_prioritary{prioritary}, m_itinerary{std::move(itinerary)} {}
 
 	const int station() const {
 	  return m_station;
 	}
+	void set_station(int station) {
+	  m_station = station;
+	}
+
 	const bool prioritary() const {
 	  return m_prioritary;
 	}
-	const std::vector<int>& itinerary() const {
+	void set_priority(bool prioritary) {
+	  m_prioritary = prioritary;
+	}
+
+	const std::string id() const {
+	  return m_id;
+	}
+	void set_id(std::string id) {
+	  m_id = id;
+	}
+
+	const std::vector<std::string>& itinerary() const {
 	  return m_itinerary;
+	}
+	void set_itinerary(std::vector<std::string> itinerary) {
+	  m_itinerary = std::move(itinerary);
+	}
+	void add_to_itinerary(const std::string& station) {
+	  m_itinerary.push_back(station);
 	}
 
 	void move(int next_station, bool station_available) {
@@ -32,11 +55,4 @@ class Train {
 	  }
 	}
 	
-	void set_itinerary(std::vector<int> itinerary) {
-	  m_itinerary = std::move(itinerary);
-	}
-
-	void add_to_itinerary(int station) {
-	  m_itinerary.push_back(station);
-	}
 };
