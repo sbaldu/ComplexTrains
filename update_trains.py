11da848d6c74960afbe30f1d24f3ca10d87aa437
Grufoony <gregorio_2000@hotmail.it>
2023-05-15 10:24:01 +0200

    updated train trips

--- update_trains.py
+++ update_trains.py
@@ -2,20 +2,13 @@ import pandas as pd
 from trainitalia import viaggiatreno as vt
 from tqdm import tqdm
 
-# df = pd.read_csv('./data/trains.txt', sep='|', names=['Type', 'ID'])
-# print(len(df))
-# df = df.drop_duplicates(subset=['ID'])
-# print(len(df))
-
-# df.to_json('./data/trains.json')
-
 df = pd.read_json('./data/trains.json')
 print(df.head())
-df['Trip'] = [[] for i in range(len(df))]
+# df['Trip'] = [[] for i in range(len(df))]
 
 for index, row in tqdm(df.iterrows(), total=len(df)):
     try:
-        if row['Type'] == 'Regionale':
+        if len(row['Trip']) == 0:
             number, station, date = vt.get_train_start(row['ID'])
             stops = vt.trip_info(station, row['ID'])
             trip = []
@@ -25,7 +18,4 @@ for index, row in tqdm(df.iterrows(), total=len(df)):
     except:
         pass
 
-df.to_json('./data/trains2.json')
-
-# df = pd.read_json('./data/trains.json')
-# print(df.head())
+df.to_json('./data/trains.json')
