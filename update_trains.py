83a6a7e4b5630db2bbc55d1287dd0fd1eb975ff1
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-05-16 13:35:53 +0200

    fix #1

--- update_trains.py
+++ update_trains.py
@@ -4,17 +4,19 @@ from tqdm import tqdm
 
 df = pd.read_json('./data/trains.json')
 print(df.head())
-# df['Trip'] = [[] for i in range(len(df))]
+df['Trip'] = [[[]] for i in range(len(df))]
 
 for index, row in tqdm(df.iterrows(), total=len(df)):
     try:
-        if len(row['Trip']) == 0:
-            number, station, date = vt.get_start_info(row['ID'])
-            stops = vt.get_trip_info(station, row['ID'])
+        trips = []
+        for train in vt.get_start_info(row['ID']):
+            number, station, date = train
+            stops = vt.get_trip_info(number, station)
             trip = []
             for stop in stops:
                 trip.append(stop['id'])
-            df.at[index, 'Trip'] = trip
+            trips.append(trip)
+        df.at[index, 'Trip'] = trips
     except:
         pass
 
